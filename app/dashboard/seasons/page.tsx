673aec7948332e718c225fa48cb211a4d66ad13a
Kurtis Kuntz <meatwad6315@gmail.com>
2024-02-02 14:52:39 -0500

    adding auth

--- app/dashboard/seasons/page.tsx
+++ app/dashboard/seasons/page.tsx
@@ -1,3 +1,37 @@
+'use client';
+
+import { useState, useEffect } from 'react';
+import { fetchSeasons } from '@/app/lib/data';
+import { lusitana } from '@/app/ui/fonts';
+
+interface Season {
+  seasonId: number;
+  name: string;
+}
+
 export default function Page() {
-    return <p>Seasons Page</p>;
-  }
+  const [seasons, setSeasons] = useState<Season[]>([]);
+
+  useEffect(() => {
+    async function getSeasons() {
+      const data = await fetchSeasons();
+      setSeasons(data);
+    }
+    getSeasons();
+  }, []);
+
+  return (
+    <main>
+      <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
+        Dashboard
+      </h1>
+      <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+        {seasons.map((season) => (
+          <div key={season.seasonId}>
+            <h2>{season.name}</h2>
+          </div>
+        ))}
+      </div>
+    </main>
+  );
+}
