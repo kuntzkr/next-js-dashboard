6d093432d0c73b94a6b30c6dcae6f7faef39002b
Your Name <you@example.com>
2024-02-07 13:55:46 -0500

    enable jwt

--- auth.config.ts
+++ auth.config.ts
@@ -6,17 +6,25 @@ export const authConfig = {
   },
   callbacks: {
     authorized({ auth, request: { nextUrl } }) {
+      
+      // Get user logged-in & page status
       const isLoggedIn = !!auth?.user;
-      const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
-      if (isOnDashboard) {
-        if (isLoggedIn) return true;
-        return false; // Redirect unauthenticated users to login page
-      } else if (isLoggedIn) {
+      const isOnSecureRoute = nextUrl.pathname.startsWith('/dashboard');
+      
+      // Case of user on a secure route - return login status
+      if (isOnSecureRoute) {
+        return isLoggedIn;
+      } 
+      
+      else if (isLoggedIn) {
         return Response.redirect(new URL('/dashboard', nextUrl));
       }
+
+      // Case of unauth'd user hitting public route
       return true;
     },
   },
   providers: [], // Add providers with an empty array for now
   secret: process.env.SECRET,
+  session: { strategy: "jwt" }, 
 } satisfies NextAuthConfig;
